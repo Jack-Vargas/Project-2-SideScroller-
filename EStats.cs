c67f9ce68cf39a3e0af4529d8364bca8e7c8cbac
Jack-Vargas <149019358+Jack-Vargas@users.noreply.github.com>
2024-07-20 14:41:59 -0700

    Add files via upload

---
+++ EStats.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EStats : MonoBehaviour
+{
+    public int health;
+    public GameObject gear;
+    public int dropNum;
+
+
+
+    void Update()
+    {
+        if (health <= 0) 
+        {
+            Drop();
+            Destroy(gameObject);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+
+    public void Drop()
+    {
+        int dropNum = Random.Range (0, 2
+            
+            
+            
+            );
+        if (dropNum == 1)
+        {
+            Instantiate(gear, gameObject.transform.position, gameObject.transform.rotation);
+        }
+    }
+
+}
